ef2b1fa40dbcdf4c732267a22877f7c837c4d8c4
Evan Halley <ehalley@bandwidth.com>
2021-08-20 14:46:33 -0400

    test

--- src/main/java/dev/evanhalley/googleprep/RotateMatrix.java
+++ src/main/java/dev/evanhalley/googleprep/RotateMatrix.java
@@ -6,30 +6,43 @@ import java.util.List;
 public class RotateMatrix {
 
     // moves, right, down, left, up
-    private final static int[][] MOVES = {{ 0, 1 }, { 1, 0 }, {0, -1}, { -1, 0}};
+    private static final int[][] MOVES = {{ 0, 1 }, { 1, 0 }, {0, -1}, { -1, 0}};
+
+    public static void main(String[] args) {
+        int[][] matrix = {  {1,2}, {3,4} };
+        new RotateMatrix().rotate(matrix);
+    }
 
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        int swapsNeeded = n * n;
-
-        int currentRow = 0;
-        int currentCol = 2;
-
-        for (int i = 0; i < swapsNeeded; i++) {
-            calculateDestinations(currentRow, currentCol, n - 1, 0, n - 1);
-        }
-
+        rotateMatrix(matrix, n - 1, 0, n - 1);
     }
 
-    public int executeSwap(int[][] matrix, int row, int col) {
-        return 0;
+    public void rotateMatrix(int[][] matrix, int rightBottomBound, int leftTopBound, int spacesToMove) {
+        int currentRow = 0;
+        int currentCol = 0;
+
+        if (spacesToMove > 0) {
+            List<Integer[]> destinations = calculateDestinations(currentRow, currentCol, rightBottomBound,
+                    leftTopBound, spacesToMove);
+            executeSwaps(matrix, destinations);
+            rotateMatrix(matrix, rightBottomBound - 1, leftTopBound + 1,
+                    spacesToMove - 2);
+        }
+    }
+
+    public void executeSwaps(int[][] matrix, List<Integer[]> destinations) {
+        int temp = matrix[destinations.get(3)[0]][destinations.get(3)[1]];
+        matrix[destinations.get(1)[0]][destinations.get(1)[1]] = matrix[destinations.get(0)[0]][destinations.get(0)[1]];
+        matrix[destinations.get(2)[0]][destinations.get(2)[1]] = matrix[destinations.get(1)[0]][destinations.get(1)[1]];
+        matrix[destinations.get(3)[0]][destinations.get(3)[1]] = matrix[destinations.get(2)[0]][destinations.get(2)[1]];
+        matrix[destinations.get(0)[0]][destinations.get(0)[1]] = temp;
     }
 
     public List<Integer[]> calculateDestinations(int row, int col, int rightBottomBound, int leftTopBound, int numMoves) {
         List<Integer[]> destinations = new ArrayList<>(numMoves);
         int tempCol = col;
         int tempRow = row;
-        int i = 0;
 
         for (int[] move : MOVES) {
             boolean moveSaved = false;
@@ -71,7 +84,6 @@ public class RotateMatrix {
                     destinations.add(new Integer[] { tempRow, tempCol });
                     moveSaved = true;
                 }
-
             }
         }
         return destinations;
